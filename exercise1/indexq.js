cbd895962a436686fa8f745ea663750a85c4ac38
mauriceaaa <9400367@gmail.com>
2019-10-08 15:34:34 +0800

    exercise1

---
+++ exercise1/indexq.js
@@ -0,0 +1,7 @@
+var _ = module.exports = {}
+
+_.head = function (array) {
+        if (array.length >= 1 )
+        return array [0]
+}       
+module.exports = _
---
+++ exercise1/test.js
@@ -0,0 +1,4 @@
+const _  = require ('indexq')
+
+console.log ('_.head=([1, 2, 3])=' , _.head=([1, 2, 3])) 
+console.log ('_.head=([])=' , _.head([]));
