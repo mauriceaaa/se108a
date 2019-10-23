6dcc990e3aa40545cd8fcc94906d92dd762fe8fe
mauriceaaa <9400367@gmail.com>
2019-10-23 09:02:17 +0800

    1016

--- exercise1016/q1.js
+++ exercise1016/q1.js
@@ -1,7 +1,10 @@
 //遞迴不查表
+var array = [0 , 1]
 function factorial(n) {
-    if (n === 0) {
-        return 1;
-    }
-    return 3 * factorial(n - 1) + 2 *factorial(n - 2);
+    if (n < 0) return 0;
+    if (array[n] != null) return array[n]
+
+    array[n] =  3 * factorial(n - 1) + 2 *factorial(n - 2);
+return array[n];
 }
+console.log("factorial(1)=",factorial(1))
--- exercise1016/q2.js
+++ exercise1016/q2.js
@@ -1,1 +1,17 @@
 //遞迴要查表
+function f (k)
+{
+    if(k==0)
+     return 0;
+    else if(k==1)
+     return 1;
+    else
+     return 3*f(k-1) + 2*f(k-2);
+}
+var startTime = Date.now()
+console.log("f(1)=",f(1))
+console.log("f(2)=",f(2))
+console.log("f(40)=",f(40))
+var endTime = Date.now()
+var milliSeconds = endTime - startTime
+console.log('time:%dms', milliSeconds)
